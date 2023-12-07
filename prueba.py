dc83cb42f0c9e3e859085e9c22b1229f1269f6d2
Nandouzc <151587252+Nandouzc@users.noreply.github.com>
2023-12-07 19:30:23 -0400

    Update

---
+++ prueba.py
@@ -0,0 +1,69 @@
+"""Probando python"""
+# pintor = "Francisco"
+# status = " Esta ocupado"
+# pintor_availability = f"{pintor} {status}"
+# print(pintor_availability.center(50))
+# print(pintor_availability.replace("ocupado","muy ocupado"))
+
+
+# n1 = int(input("ingresar primer numero"))
+# n2 = int(input("ingresar segundo numero"))
+# suma = n1 + n2
+# resta = n1 - n2
+# multi = n1 * n2
+# div = n1 / n2
+# mensaje = f""" Para los números {n1} y {n2}, 
+# El resultado de la suma es {suma}.
+# El resultado de la resta es {resta}.
+# El resultado de la multiplicación es {multi}.
+# El resultado de la división es {div}.
+# """
+# print(mensaje)
+
+
+
+# colors = ["green","blue","red"]
+# colors.append("violet")
+# colors.extend(["orange", "teal"])
+# colors.insert(2 , "pink")
+#  print(colors)
+
+
+
+# count = 1
+# while count<=10:
+#     print(count)
+#     count += 1
+
+
+
+# produccion = 0
+# for f in range(5):
+#     valor = int(input("ingrese la produccion por colmena"))
+#     produccion = produccion + valor
+# print("La produccion total es: ", produccion)
+# promedio = produccion/5
+# print("El promedio por colmena es: ", promedio)
+
+
+
+# arroba = "@"
+# cantidad = 0
+# mail = input("Ingrese su correo...")
+# for f in mail:
+#     if f==arroba:
+#         cantidad += 1
+# if cantidad>=1: 
+#     print("Su correo es", mail)   
+# else :
+#     print("El correo que ingresó no es correcto")
+
+
+# valor = int(input("Ingrese el numero de la tabla"))
+# if valor<=10 :
+#     print("Selecciono la tabla del",valor)
+#     for multi in range(11) :
+#         print(valor, "X", multi, "=", multi*valor  )
+# else :
+#     print("La tabla que selecciono no es correcta")    
+
--- python.py
+++ python.py
@@ -1,23 +1,30 @@
-"""Module providing a function printing python version."""
+"""Python"""
 usuario = input("Ingrese usuario")
-clave = input("Ingrese el codigo")
+codigo = input("Ingrese el codigo")
 botellas_miel = float(input("Cuantas botellas de miel desea?"))
 precio = float(input("De que precio desea?"))
 total = botellas_miel * precio
-print("Para", usuario, "el codigo es", clave, usuario, "lleva ",
+print("Para", usuario, "el codigo es", codigo, usuario, "lleva ",
       botellas_miel, " botellas de miel", "El precio a pagar es",
       total)
 
-pago = int(input(
-    "seleccione metodo de pago: Para tarjeta presione 1, para efectivo presione 2, para transferencia presione 3"))
-if pago == 1:
-    tarjeta = int(input("introduzca su tarjeta"))
-    print(tarjeta)
-elif pago == 2:
-    print("Por favor cancelar en su distribuidor mas cercano")
-elif pago == 3:
-    referencia_bancaria = int(
+finish= False
+while finish is False:
+    pago = int(input(
+    """seleccione metodo de pago: Para tarjeta presione 1,
+      para efectivo presione 2, para transferencia presione 3"""))
+    if pago == 1:
+        tarjeta = int(input("introduzca su tarjeta"))
+        print("Operacion exitosa")
+        finish= True
+    elif pago == 2:
+        print("Por favor cancelar en su dis tribuidor mas cercano")
+        print("Esperamos su visita")
+        finish= True
+    elif pago == 3:
+        referencia_bancaria = int(
         input("Introduzca el numero de su referencia bancaria "))
-    print("La referencia es", referencia_bancaria)
-else:
-    print("La opción que marcó no es correcta")
+        print("La referencia es", referencia_bancaria)
+        finish= True
+    else:
+        print("La opción que marcó no es correcta")
