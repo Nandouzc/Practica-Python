cb22c997552343c67437ef464d3816788779d32f
Nandouzc <151587252+Nandouzc@users.noreply.github.com>
2023-12-08 18:39:43 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,20 +1,20 @@
 """Python"""
-usuario = input("Ingrese usuario")
-codigo = input("Ingrese el codigo")
-botellas_miel = float(input("Cuantas botellas de miel desea?"))
-precio = float(input("De que precio desea?"))
+usuario = input("Ingrese usuario: ")
+codigo = input("Ingrese el codigo: ")
+botellas_miel = float(input("Cuantas botellas de miel desea?: "))
+precio = float(input("De que precio desea?: "))
 total = botellas_miel * precio
-print("Para", usuario, "el codigo es", codigo, usuario, "lleva ",
-      botellas_miel, " botellas de miel", "El precio a pagar es",
+print("Para", usuario, "el codigo es", codigo,",", usuario, "lleva ",
+      botellas_miel, " botellas de miel",",", "El precio a pagar es",
       total)
 
 finish= False
 while finish is False:
     pago = int(input(
     """seleccione metodo de pago: Para tarjeta presione 1,
-      para efectivo presione 2, para transferencia presione 3"""))
+      para efectivo presione 2, para transferencia presione 3 """))
     if pago == 1:
-        tarjeta = int(input("introduzca su tarjeta"))
+        tarjeta = int(input("introduzca su tarjeta "))
         print("Operacion exitosa")
         finish= True
     elif pago == 2:
@@ -27,4 +27,4 @@ while finish is False:
         print("La referencia es", referencia_bancaria)
         finish= True
     else:
-        print("La opción que marcó no es correcta")
+        print("La opción que marcó no es correcta ")
